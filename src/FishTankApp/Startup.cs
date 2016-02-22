16e4beea0ef942b7d31b906f5faade1eea83b554
mpatal <marvin.patal@hotmail.com>
2016-02-21 22:19:47 -0500

    Adding logging, mvc, static files and status code page services.

--- src/FishTankApp/Startup.cs
+++ src/FishTankApp/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace FishTankApp
 {
@@ -12,18 +13,28 @@ namespace FishTankApp
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddSingleton<ISensorDataService, SensorDataService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(LogLevel.Information);
+            var logger = loggerFactory.CreateLogger<Startup>();
             app.UseIISPlatformHandler();
 
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new {controller = "Home", action = "Index"}
+                    );
             });
+
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
         }
 
         // Entry point for the application.
